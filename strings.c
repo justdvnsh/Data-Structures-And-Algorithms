8c2b79a00e526a1f06821411c767db889a6fd991
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2019-12-19 15:25:14 +0530

    added string.c

--- strings.c
+++ strings.c
@@ -4,6 +4,13 @@
 
 int main() {
 
+    // every character represents a particular number
+    // lowercase character represents particular set of numbers , while uppercase characters represents different set of numbers.
+    char c;
+    for (int i = 'a' ; i <= 'z' ; i++) {
+        printf("%c -> %d \n", i, i);
+    }
+
     // so as we know a string is just an array of characters. So, every thing we can do with array , we can do with strings.
     // here is how it is defined.
     char str[11];
@@ -19,12 +26,25 @@ int main() {
     str[4] = 'j';
     str[5] = 'k';
 
-    printf("%s", str); // %s is for strings,. // prints abc, since 4th element marks the end.
+    printf("%s \n", str); // %s is for strings,. // prints abc, since 4th element marks the end.
 
     /*
     * NOTE - double quotes are not allowed to store characters. "";
-    * One must use the
+    * One must use the single quotes since , double quotes are used for strings.
     */
+
+    // so , we must always include one extra space for '\0'
+    char string1[8] = "divyansh";
+    printf("String with 8 elements -> %s \n", string1); // it prints -> divyanshabc, since abc was the last string program worked with.
+    // since , the string was 8 characters long , and we only alloted 8 characters , no space for '\0'
+    char string2[9] = "divyansh";
+    printf("String with 9 elements -> %s \n", string2); // prints divyansh , since it knows now where to end and has a particular
+    // space for the end char.
+
+    // the string.h library provides many useful functions to work for string.
+    // strcmp -> to compare two strings.
+    int result = strcmp(string1, string2);
+    printf("The second string differs from the first string by %c letters. \n", result);
 
     return 0;
 }
